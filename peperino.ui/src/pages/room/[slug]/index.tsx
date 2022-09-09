9f8780b3d7082c9e1ef5e4be77ef4af1c4809441
Dennis Kaminski <dennis.kaminski95@gmail.com>
2022-09-10 00:54:07 +0200

    prepare room page

--- peperino.ui/src/pages/room/[slug]/index.tsx
+++ peperino.ui/src/pages/room/[slug]/index.tsx
@@ -1,7 +1,9 @@
+import { Favorite, LocalActivity, Restore } from "@mui/icons-material";
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 import { GetServerSideProps } from "next";
-import { useEffect } from "react";
-import { RoomOutDto } from "../../../lib/api";
-import { withAuth } from "../../../lib/auth/server/authPage";
+import { useEffect, useState } from "react";
+import { RoomOutDto, RoomService } from "../../../lib/api";
+import { authPage, redirectLogin } from "../../../lib/auth/server/authPage";
 import { KnownRoutes } from "../../../lib/routing/knownRoutes";
 import { useApplicationState } from "../../../lib/state/ApplicationState";
 
@@ -43,8 +45,32 @@ const GroupPage = (props: Props) => {
         groupPageState.updateToolbar();
     }, [])
 
+    const [bottomNavigation, setBottomNavigation] = useState(0);
+
     return (
-        <>Group Page {props.room?.roomName} </>
+        <>
+            <div style={{ minHeight: "calc(100% - 60px - 56px)", paddingBottom: "60px" }}>
+                <>Group Page {props.room?.roomName} </>
+                {"TEXT"}
+                <>TEXT</>
+            </div>
+            <BottomNavigation
+                sx={{
+                    position: "sticky",
+                    bottom: "0px",
+                    width: "100%"
+                }}
+                showLabels
+                value={bottomNavigation}
+                onChange={(event, newValue) => {
+                    setBottomNavigation(newValue);
+                }}
+            >
+                <BottomNavigationAction label="Recents" icon={<Restore />} />
+                <BottomNavigationAction label="Favorites" icon={<Favorite />} />
+                <BottomNavigationAction label="Nearby" icon={<LocalActivity />} />
+            </BottomNavigation>
+        </>
     );
 }
 
