c56a67be0b788b99d357172e6040474b131ffbfe
Dennis Kaminski <dennis.kaminski95@gmail.com>
2022-09-27 23:11:57 +0200

    Expose server env

---
+++ Peperino/Controllers/EnvironmentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace Peperino.Controllers
+{
+    [Authorize]
+    public class EnvironmentController : ApiControllerBase
+    {
+        public EnvironmentController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<IDictionary> GetEnvironment()
+        {
+            var variables = Environment.GetEnvironmentVariables();
+            return Ok(variables);
+        }
+    }
+}
